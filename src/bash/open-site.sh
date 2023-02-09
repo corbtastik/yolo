0ae0d20fdfabe4cb67f81d07e2d1e38d46a7dac6
corbs <corbtastik@gmail.com>
2023-02-08 18:37:29 -0600

    open-site refactor

--- src/bash/open-site.sh
+++ src/bash/open-site.sh
@@ -96,12 +96,12 @@ echo "======================================================================="
 #------------------------------------------------------------------------------
 # Execute save commands
 #------------------------------------------------------------------------------
-cp ./index.md $(echo $SAVE_INDEX_FILE)
-cp ./_config.yml $(echo $SAVE_CONFIG_FILE)
-cp $(echo $SAVE_THEME_FILE) ./src/samples/$SAVE_SITE/_sass/yolo/themes/
-cp $(echo $SAVE_DATA_FILES) ./src/samples/$SAVE_SITE/_data/
-cp $(echo $SAVE_IMAGE_FILES) ./src/samples/$SAVE_SITE/assets/images/
-cp ./yolo.json ./src/samples/$SAVE_SITE/
+mv ./index.md $(echo $SAVE_INDEX_FILE)
+mv ./_config.yml $(echo $SAVE_CONFIG_FILE)
+mv $(echo $SAVE_THEME_FILE) ./src/samples/$SAVE_SITE/_sass/yolo/themes/
+mv $(echo $SAVE_DATA_FILES) ./src/samples/$SAVE_SITE/_data/
+mv $(echo $SAVE_IMAGE_FILES) ./src/samples/$SAVE_SITE/assets/images/
+mv ./yolo.json ./src/samples/$SAVE_SITE/
 #------------------------------------------------------------------------------
 # Execute open commands
 #------------------------------------------------------------------------------
